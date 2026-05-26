5e5bcfed8b18d6c0c84dd6140049aec2d80c6214
faisal <faisal@faisals-MacBook-Air.local>
2026-05-26 16:02:30 +0300

    add the fucntion BoardInit, but need fixing for the whole board

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
-# Example file showing a basic pygame "game loop"
 import pygame
 
-# pygame setup
+
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((640, 640))
 clock = pygame.time.Clock()
 running = True
 
+
+def BoardInit(): # to init the board outside of the game loop 
+    
+    weigth = 80 
+    height = 80
+
+    for i in range(8):
+
+        currentX = i * 80
+        currentY = i * 80
+
+        if i % 2 == 0: # even
+            pygame.draw.rect(screen, "black", (currentX , currentY , weigth ,height))
+        else:
+            pygame.draw.rect(screen, "white", (currentX , currentY , weigth ,height))
+        
+
+
+
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    # RENDER YOUR GAME HERE
-
-    # flip() the display to put your work on screen
+    screen.fill("gray")
+    BoardInit()
+    
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(60)  # FPS 
 
 pygame.quit()
