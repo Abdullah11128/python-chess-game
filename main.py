34dd38458b0728eb947e1c71c58ad3d3c4c0ee86
Abdullah11128 <blmny417@gmail.com>
2026-05-26 21:14:39 +0300

    Make broad
    Updated screen size and added BoardInit function to draw a checkerboard pattern.

--- main.py
+++ main.py
@@ -1,27 +1,52 @@
-# Example file showing a basic pygame "game loop"
 import pygame
 
-# pygame setup
+
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((640, 640))
 clock = pygame.time.Clock()
 running = True
 
+
+def BoardInit(): # to init the board outside of the game loop 
+    
+    weigth = 80 
+    height = 80
+    currentX = 0
+    currentY = 0
+    switch1=1
+    for i in range(8):
+        currentX = 0
+        switch1+=1
+        for j in range(8):
+            if  switch1%2==0: # even
+                pygame.draw.rect(screen, "black", (currentX , currentY , weigth ,height))
+                
+                
+            else:
+                pygame.draw.rect(screen, "white", (currentX , currentY , weigth ,height))
+            currentX += 80
+            switch1+=1 
+        currentY += 80
+        
+
+
+        
+
+        
+
+
+
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    # RENDER YOUR GAME HERE
-
-    # flip() the display to put your work on screen
+    screen.fill("gray")
+    BoardInit()
+    
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(60)  # FPS 
 
 pygame.quit()
