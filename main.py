b1d3c9cb8f64d9b3768193414971e14869202826
faisal <faisal@faisals-MacBook-Air.local>
2026-05-28 18:48:41 +0300

    map the chess board and Experiment with a loop

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ screen = pygame.display.set_mode((640, 640))
 clock = pygame.time.Clock()
 running = True
 
+Matrix = [["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],  
+    ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],  
+    ["--", "--", "--", "--", "--", "--", "--", "--"],  
+    ["--", "--", "--", "--", "--", "--", "--", "--"],  
+    ["--", "--", "--", "--", "--", "--", "--", "--"],  
+    ["--", "--", "--", "--", "--", "--", "--", "--"],  
+    ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+    ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+
+
+
+OG_blackqueen = pygame.image.load("assets/Chess_queenB.bmp")
+scaledQueen = pygame.transform.scale(OG_blackqueen , (70 , 70))
 
 def BoardInit(): # to init the board outside of the game loop 
     
@@ -14,25 +27,25 @@ def BoardInit(): # to init the board outside of the game loop
     currentX = 0
     currentY = 0
     switch1=1
+
     for i in range(8):
         currentX = 0
         switch1+=1
+
         for j in range(8):
             if  switch1%2==0: # even
-                pygame.draw.rect(screen, "black", (currentX , currentY , weigth ,height))
-                
-                
+                pygame.draw.rect(screen, (240, 217, 181), (currentX , currentY , weigth ,height))
+            
+            if Matrix[i][j] == "bR":
+                    screen.blit(scaledQueen , (currentX , currentY))
             else:
-                pygame.draw.rect(screen, "white", (currentX , currentY , weigth ,height))
+                pygame.draw.rect(screen, (118, 150, 86), (currentX , currentY , weigth ,height))
+
             currentX += 80
-            switch1+=1 
+            switch1+=1
+    
         currentY += 80
-        
-
-
-        
-
-        
+    
 
 
 
@@ -44,6 +57,7 @@ while running:
 
     screen.fill("gray")
     BoardInit()
+
     
     pygame.display.flip()
 
