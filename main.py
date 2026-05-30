bf9b23b9c94968dd254072d37d7f0ab1ea5166c7
faisal12-12 <195893424+faisal12-12@users.noreply.github.com>
2026-05-30 21:17:30 +0300

    add pieces plcaing function , finishing the board and pieces phase

--- main.py
+++ main.py
@@ -17,8 +17,24 @@ Matrix = [["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
 
 
 
-OG_blackqueen = pygame.image.load("assets/Chess_queenB.bmp")
-scaledQueen = pygame.transform.scale(OG_blackqueen , (70 , 70))
+OG_blackqueen = pygame.image.load("assets/B_Rook.bmp")
+scaledQueen = pygame.transform.scale(OG_blackqueen , ( 80 , 80))
+
+OG_blcackKnight = pygame.image.load("assets/B_Knight.bmp")
+scaleB_Knight = pygame.transform.scale(OG_blcackKnight , (80 , 80 ))
+
+OG_WightKnight = pygame.image.load("assets/W_Knight.bmp")
+scaleW_Knight = pygame.transform.scale(OG_WightKnight , (70 , 70 ))
+
+
+def pieces(P,currentX , currentY):
+    match P:
+        case "bR":
+            screen.blit(scaledQueen , (currentX , currentY))
+        case "bN":
+            screen.blit(scaleB_Knight , (currentX , currentY))
+        case "wN":
+            screen.blit(OG_WightKnight , (currentX , currentY))
 
 def BoardInit(): # to init the board outside of the game loop 
     
@@ -35,17 +51,15 @@ def BoardInit(): # to init the board outside of the game loop
         for j in range(8):
             if  switch1%2==0: # even
                 pygame.draw.rect(screen, (240, 217, 181), (currentX , currentY , weigth ,height))
-            
-            if Matrix[i][j] == "bR":
-                    screen.blit(scaledQueen , (currentX , currentY))
             else:
                 pygame.draw.rect(screen, (118, 150, 86), (currentX , currentY , weigth ,height))
+    
+            pieces( Matrix[i][j] , currentX , currentY )
 
             currentX += 80
             switch1+=1
     
         currentY += 80
-    
 
 
 
