b0e42da901a3eb79b7dc30154d007aed9abab930
faisal12-12 <195893424+faisal12-12@users.noreply.github.com>
2026-05-30 22:21:25 +0300

    completed  one side need fixing in size and the other need to be done

--- Board.py
+++ Board.py
@@ -17,24 +17,46 @@ Matrix = [["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
 
 
 
-OG_blackqueen = pygame.image.load("assets/B_Rook.bmp")
-scaledQueen = pygame.transform.scale(OG_blackqueen , ( 80 , 80))
+OG_blackRock = pygame.image.load("assets/B_Rook.png")
+scaled_BRock = pygame.transform.scale(OG_blackRock , ( 40 , 80))
 
-OG_blcackKnight = pygame.image.load("assets/B_Knight.bmp")
-scaleB_Knight = pygame.transform.scale(OG_blcackKnight , (80 , 80 ))
+OG_blcackKnight = pygame.image.load("assets/B_Knight.png")
+scaleB_Knight = pygame.transform.scale(OG_blcackKnight , (40 , 80 ))
 
-OG_WightKnight = pygame.image.load("assets/W_Knight.bmp")
-scaleW_Knight = pygame.transform.scale(OG_WightKnight , (70 , 70 ))
+OG_B_bishop = pygame.image.load("assets/B_Bishop.png")
+scaleB_Bishop = pygame.transform.scale(OG_B_bishop , (40 , 80 ))
+
+OG_B_queen = pygame.image.load("assets/B_Queen.png")
+scaleB_Queen = pygame.transform.scale(OG_B_queen , (40 , 80 ))
+
+OG_B_King = pygame.image.load("assets/B_King.png")
+scaleB_King = pygame.transform.scale(OG_B_King , (40 , 80 ))
+
+OG_B_pawn = pygame.image.load("assets/B_Pawn.png")
+scaleB_Pawn = pygame.transform.scale(OG_B_pawn , (40 , 80 ))
+
+OG_WightKnight = pygame.image.load("assets/W_Knight.png")
+scaleW_Knight = pygame.transform.scale(OG_WightKnight , (40 , 80 ))
+
+
 
 
 def pieces(P,currentX , currentY):
     match P:
         case "bR":
-            screen.blit(scaledQueen , (currentX , currentY))
+            screen.blit(scaled_BRock , (currentX , currentY))
         case "bN":
             screen.blit(scaleB_Knight , (currentX , currentY))
+        case "bB":
+            screen.blit(scaleB_Bishop , (currentX , currentY))
+        case "bQ":
+            screen.blit(scaleB_Queen , (currentX , currentY))
+        case "bK":
+            screen.blit(scaleB_King , (currentX , currentY))
+        case "bP":
+            screen.blit(scaleB_Pawn , (currentX , currentY ))
         case "wN":
-            screen.blit(OG_WightKnight , (currentX , currentY))
+            screen.blit(scaleW_Knight , (currentX , currentY))
 
 def BoardInit(): # to init the board outside of the game loop 
     
